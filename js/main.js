2cf350b88c36bd2865bb2e6f4f870bce1daf3943
Eric Jiang <jbneric@Eric-Jiangs-MacBook-Air.local>
2015-05-28 13:29:16 -0700

    Changed getParameter function to return lowerCase metal string

--- js/main.js
+++ js/main.js
@@ -200,7 +200,7 @@ function getParameter(name) {
 	name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
 	var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
 		results = regex.exec(location.search);
-	return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+	return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " ")).toLowerCase();
 }
 
 $(window).load(function() {
