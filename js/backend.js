800156a2a0eb479c15036080d951ccb4a54017bb
Eric Jiang <jbneric@Eric-Jiangs-MacBook-Air.local>
2015-05-28 13:26:37 -0700

    Inventory.html my-stack queries from DB

--- js/backend.js
+++ js/backend.js
@@ -1,5 +1,68 @@
 Parse.initialize("AExU8zqOb8xQlqLVykAzD3CyD2YfQmzJM41lOyj7", "lqsaTVz8JWchE92g8GDbGb6SzfrKmJaKOqIiFTeK");
 
+function loadMyStack() {
+    var tbody = document.getElementById('my-stack-inventory');
+    var purityHeader = document.getElementById('purity-header');
+    var metal = getParameter('metal');
+    if (metal === '') {
+        metal = 'gold';
+    }
+    // alert(metal);
+    switch (metal) {
+        case 'gold':
+            purityHeader.innerHTML = "% au";
+            break;
+        case 'silver':
+            purityHeader.innerHTML = "% ag";
+            break;
+        case 'platinum':
+            purityHeader.innerHTML = "% pt";
+            break;
+        default:
+            purityHeader.innerHTML = "% au";
+    }
+    var Bullion = Parse.Object.extend("Bullion");
+    var query = new Parse.Query(Bullion);
+    query.equalTo("metal", metal);
+    query.find({
+      success: function(results) {
+        // alert("Successfully retrieved " + results.length + " bullions.");
+        if (results.length === 0) {
+            tbody.innerHTML = "<tr><td>None</td></tr>";
+        }
+        else {
+            for (var i = 0; i < results.length; i++) {
+                var bullion = results[i];
+                var newRow = tbody.insertRow(tbody.rows.length);
+
+                var image = newRow.insertCell(newRow.cells.length);
+                image.className = 'stack-img-col';
+                image.innerHTML = '<div class="coin_mini"></div>';
+
+                var name = newRow.insertCell(newRow.cells.length);
+                //name.innerHTML = '<a href="view.html">' + bullion.get('name') + '</a>';
+                name.innerHTML = '<a href="view.html"></a>' + bullion.get('name');
+
+                var quantity = newRow.insertCell(newRow.cells.length);
+                quantity.innerHTML = bullion.get('quantity');
+
+                var weight = newRow.insertCell(newRow.cells.length);
+                weight.innerHTML = bullion.get('weight');
+
+                var purity = newRow.insertCell(newRow.cells.length);
+                purity.innerHTML = bullion.get('purity');
+
+                var value = newRow.insertCell(newRow.cells.length);
+                value.innerHTML = bullion.get('unitPrice');
+            }
+        }
+      },
+      error: function(error) {
+        alert("Error: " + error.code + " " + error.message);
+      }
+    });
+}
+
 function signupPressed(event) {
     if (window.event) {
         event = window.event;
